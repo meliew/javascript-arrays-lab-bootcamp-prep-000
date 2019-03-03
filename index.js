f6939b1fdc69f82ce3ff5744c89de6fa72be724b
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-03 23:44:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(array, kitten) {
+
+kittens.push("Ralph");
+
+}
+
+function destructivelyPrependKitten(array, kitten) {
+
+  kittens.unshift("Bob")
+
+}
+
+function destructivelyRemoveLastKitten(array) {
+kittens.pop();
+
+}
+
+function destructivelyRemoveFirstKitten(array) {
+  kittens.shift();
+  
+}
