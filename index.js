c90b1161e7f92f47cd0e1690ac123fc77ce672bb
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-03 23:52:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 function destructivelyAppendKitten(array, kitten) {
 
-  array.push("Ralph");
+  kittens.push("Ralph");
 
 }
 
